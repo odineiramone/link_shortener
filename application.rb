14610a16839da63a54e7abad94a3aaef5bc9d380
OdineiRibeiro <odinei.ribeiro@live.com>
2018-02-20 22:59:37 -0300

    add 'http://' on link if it doesn't have. Without it the redirect not work

--- application.rb
+++ application.rb
@@ -12,9 +12,15 @@ end
 
 namespace '/api' do
   post '/short_url' do
+    complete_link = if params[:complete_link].starts_with? 'http'
+                      params[:complete_link]
+                    else
+                      'http://' + params[:complete_link]
+                    end
+
     attributes = {
       code: SecureRandom.hex(3),
-      original_url: params[:complete_link],
+      original_url: complete_link,
       request_ip: request.ip
     }
 
